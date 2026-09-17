0c90fdce2f4e01dda001b8f6af24568c53d0b4cf
temitopeolabode-ux <temitopeolabode98@gmail.com>
2026-09-17 11:26:57 +0100

    Add website files

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("script.js loaded");
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navbar = document.querySelector(".navbar");
+
+if (menuToggle && navbar) {
+  menuToggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+    const expanded = menuToggle.getAttribute("aria-expanded") === "true";
+    menuToggle.setAttribute("aria-expanded", String(!expanded));
+  });
+}
